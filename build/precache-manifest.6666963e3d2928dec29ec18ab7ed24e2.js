2fdcb3c83a75d38f98f7b8d4a70884f0b7aa8ed4
QueaT <43156293+QueaT@users.noreply.github.com>
2018-11-17 15:03:09 +0100

    fsde
    rwerw

---
+++ build/precache-manifest.6666963e3d2928dec29ec18ab7ed24e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d2f259a9aaddf24a77c8",
+    "url": "/static/js/main.d2f259a9.chunk.js"
+  },
+  {
+    "revision": "ecc97bab542667995fc7",
+    "url": "/static/js/1.ecc97bab.chunk.js"
+  },
+  {
+    "revision": "d2f259a9aaddf24a77c8",
+    "url": "/static/css/main.e45ccdfb.chunk.css"
+  },
+  {
+    "revision": "f2c24ad71fec0e25ea2c72063c0aad58",
+    "url": "/index.html"
+  }
+];
